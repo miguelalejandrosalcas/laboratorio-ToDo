2994593d14e7779c0c17abeaef5af3bc76cf4566
Miguel Alejandro <miguelalejandrosalcas@gmail.com>
2026-06-05 20:29:10 -0500

    Subiendo los archivos del lab

---
+++ script.js
@@ -0,0 +1,18 @@
+let resultado = confirm("¿Estás seguro de eliminar la tarea?");
+let mostrarMensaje = document.getElementById('error');
+let btnUrgente = document.getElementById('btn-urgente')
+
+function confirmar() {
+    if (resultado === true) {
+        mostrarMensaje.style.display = "block";
+        mostrarMensaje.innerText = "Correcto";
+    } else {
+        mostrarMensaje.style.display = "block";
+        mostrarMensaje.innerText = "Incorrecto";
+    }
+    
+}
+
+btnUrgente.addEventListener('click', function() {
+    btnUrgente.style.backgroundColor = "red";
+})
